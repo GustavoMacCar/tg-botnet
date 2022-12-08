8c3a9e4dc9d12d5e768b0aa5ed45da879b3c0364
GustavoMacCar <gmcmacar@gmail.com>
2022-12-08 17:13:05 -0300

    knn and random forest models and metrics

--- model.py
+++ model.py
@@ -1,186 +1,93 @@
-import tensorflow as tf
 import numpy as np
 import pandas as pd
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.layers import IntegerLookup
-from tensorflow.keras.layers import Normalization
-from tensorflow.keras.layers import StringLookup
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, auc, accuracy_score, precision_score, recall_score
+
+#random forest
+#naive bayes
+#ada boost
+#CNN
+#decision tree
+#svm
+
+# 1 no mesmo dataset (Domínio simples)
+# 2 entre datasets diferentes (Domínio misto/adaptado)
+# 3 misturar datasets (Domínio expandido)
+
+#auc
+#f1 score
+#recall
+#acurácia
+#precision
+
+features = pd.read_csv('ctu13_3.csv')
+labels = np.array(features['Botnet'])
+
+features = features.drop('Botnet', axis = 1)
+
+# features = features.drop(features.columns[0:28], axis=1)
+
+feature_list = list(features.columns)
+features = np.array(features)
+
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
+
+rf = RandomForestClassifier(n_estimators = 1000, random_state = 42)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+rf.fit(train_features, train_labels)
+knn.fit(train_features, train_labels)
+
+knn_predictions = knn.predict(test_features)
+rf_predictions = rf.predict(test_features)
+
+knn_errors = abs(knn_predictions - test_labels)
+# print(round(np.mean(knn_errors), 2))
+
+rf_errors = abs(rf_predictions - test_labels)
+# print(round(np.mean(rf_errors), 2))
+
+knn_f1 = f1_score(test_labels, knn_predictions)
+knn_accuracy = accuracy_score(test_labels, knn_predictions)
+knn_precision = precision_score(test_labels, knn_predictions)
+knn_recall = recall_score(test_labels, knn_predictions)
+
+rf_f1 = f1_score(test_labels, rf_predictions)
+rf_accuracy = accuracy_score(test_labels, rf_predictions)
+rf_precision = precision_score(test_labels, rf_predictions)
+rf_recall = recall_score(test_labels, rf_predictions)
+
+print(f"knn f1: {knn_f1}")
+print(f"knn accuracy: {knn_accuracy}")
+print(f"knn precision: {knn_precision}")
+print(f"knn recall: {knn_recall}")
+
+print("\n")
+
+print(f"rf f1: {rf_f1}")
+print(f"rf accuracy: {rf_accuracy}")
+print(f"rf precision: {rf_precision}")
+print(f"rf recall: {rf_recall}")
 
 
 
 
-#Preparing the data
-
-file_url = "http://storage.googleapis.com/download.tensorflow.org/data/heart.csv"
-dataframe = pd.read_csv(file_url)
-
-#print(dataframe.shape)
-
-#print(dataframe.head())
-
-val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
-train_dataframe = dataframe.drop(val_dataframe.index)
-
-print(
-    "Using %d samples for training and %d for validation"
-    % (len(train_dataframe), len(val_dataframe))
-)
-
-def dataframe_to_dataset(dataframe):
-    dataframe = dataframe.copy()
-    labels = dataframe.pop("target")
-    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-    ds = ds.shuffle(buffer_size=len(dataframe))
-    return ds
 
 
-train_ds = dataframe_to_dataset(train_dataframe)
-val_ds = dataframe_to_dataset(val_dataframe)
-
-train_ds = train_ds.batch(32)
-val_ds = val_ds.batch(32)
-
-#feature preprocessing with keras layers
-
-def encode_numerical_feature(feature, name, dataset):
-    # Create a Normalization layer for our feature
-    normalizer = Normalization()
-
-    # Prepare a Dataset that only yields our feature
-    feature_ds = dataset.map(lambda x, y: x[name])
-    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
-
-    # Learn the statistics of the data
-    normalizer.adapt(feature_ds)
-
-    # Normalize the input feature
-    encoded_feature = normalizer(feature)
-    return encoded_feature
 
 
-def encode_categorical_feature(feature, name, dataset, is_string):
-    lookup_class = StringLookup if is_string else IntegerLookup
-    # Create a lookup layer which will turn strings into integer indices
-    lookup = lookup_class(output_mode="binary")
 
-    # Prepare a Dataset that only yields our feature
-    feature_ds = dataset.map(lambda x, y: x[name])
-    feature_ds = feature_ds.map(lambda x: tf.expand_dims(x, -1))
 
-    # Learn the set of possible string values and assign them a fixed integer index
-    lookup.adapt(feature_ds)
 
-    # Turn the string input into integer indices
-    encoded_feature = lookup(feature)
-    return encoded_feature
 
-#build a model
 
-# Categorical features encoded as integers
-sex = keras.Input(shape=(1,), name="sex", dtype="int64")
-cp = keras.Input(shape=(1,), name="cp", dtype="int64")
-fbs = keras.Input(shape=(1,), name="fbs", dtype="int64")
-restecg = keras.Input(shape=(1,), name="restecg", dtype="int64")
-exang = keras.Input(shape=(1,), name="exang", dtype="int64")
-ca = keras.Input(shape=(1,), name="ca", dtype="int64")
 
-# Categorical feature encoded as string
-thal = keras.Input(shape=(1,), name="thal", dtype="string")
 
-# Numerical features
-age = keras.Input(shape=(1,), name="age")
-trestbps = keras.Input(shape=(1,), name="trestbps")
-chol = keras.Input(shape=(1,), name="chol")
-thalach = keras.Input(shape=(1,), name="thalach")
-oldpeak = keras.Input(shape=(1,), name="oldpeak")
-slope = keras.Input(shape=(1,), name="slope")
 
-all_inputs = [
-    sex,
-    cp,
-    fbs,
-    restecg,
-    exang,
-    ca,
-    thal,
-    age,
-    trestbps,
-    chol,
-    thalach,
-    oldpeak,
-    slope,
-]
 
-# Integer categorical features
-sex_encoded = encode_categorical_feature(sex, "sex", train_ds, False)
-cp_encoded = encode_categorical_feature(cp, "cp", train_ds, False)
-fbs_encoded = encode_categorical_feature(fbs, "fbs", train_ds, False)
-restecg_encoded = encode_categorical_feature(restecg, "restecg", train_ds, False)
-exang_encoded = encode_categorical_feature(exang, "exang", train_ds, False)
-ca_encoded = encode_categorical_feature(ca, "ca", train_ds, False)
 
-# String categorical features
-thal_encoded = encode_categorical_feature(thal, "thal", train_ds, True)
 
-# Numerical features
-age_encoded = encode_numerical_feature(age, "age", train_ds)
-trestbps_encoded = encode_numerical_feature(trestbps, "trestbps", train_ds)
-chol_encoded = encode_numerical_feature(chol, "chol", train_ds)
-thalach_encoded = encode_numerical_feature(thalach, "thalach", train_ds)
-oldpeak_encoded = encode_numerical_feature(oldpeak, "oldpeak", train_ds)
-slope_encoded = encode_numerical_feature(slope, "slope", train_ds)
 
-all_features = layers.concatenate(
-    [
-        sex_encoded,
-        cp_encoded,
-        fbs_encoded,
-        restecg_encoded,
-        exang_encoded,
-        slope_encoded,
-        ca_encoded,
-        thal_encoded,
-        age_encoded,
-        trestbps_encoded,
-        chol_encoded,
-        thalach_encoded,
-        oldpeak_encoded,
-    ]
-)
-x = layers.Dense(32, activation="relu")(all_features)
-x = layers.Dropout(0.5)(x)
-output = layers.Dense(1, activation="sigmoid")(x)
-model = keras.Model(all_inputs, output)
-model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
 
-# `rankdir='LR'` is to make the graph horizontal.
-keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
-
-#train the model
-model.fit(train_ds, epochs=50, validation_data=val_ds)
-
-#Inference on new data
-sample = {
-    "age": 60,
-    "sex": 1,
-    "cp": 1,
-    "trestbps": 145,
-    "chol": 233,
-    "fbs": 1,
-    "restecg": 2,
-    "thalach": 150,
-    "exang": 0,
-    "oldpeak": 2.3,
-    "slope": 3,
-    "ca": 0,
-    "thal": "fixed",
-}
-
-input_dict = {name: tf.convert_to_tensor([value]) for name, value in sample.items()}
-predictions = model.predict(input_dict)
-
-print(
-    "This particular patient had a %.1f percent probability "
-    "of having a heart disease, as evaluated by our model." % (100 * predictions[0][0],)
-)
